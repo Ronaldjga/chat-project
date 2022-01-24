9d083867443466a73f3614b11a9ff03d205beaf9
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-01-24 16:26:52 -0300

    new commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88431a8b18bc1ed8"; }
+/******/ 		__webpack_require__.h = function() { return "0a3718512f9c3da0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
